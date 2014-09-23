23cb7a0985a3d8dd9e2f4096c1537ca15d9cf14c
Eric <ericmlazarski@lewisu.edu>
2014-09-23 14:15:25 -0500

    Second Commit

---
+++ Theatre/Theatre/Source.cpp
@@ -0,0 +1,17 @@
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+	int total = 0;
+	int userChoice;
+	int prices[15];
+
+	do {
+
+	} while (userChoice != 5);
+
+
+
+}
