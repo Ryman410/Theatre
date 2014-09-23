22dc5509aa0d8c7fb20669abe947473a4a0df35b
Eric <ericmlazarski@lewisu.edu>
2014-09-23 14:25:11 -0500

    Third Commit

--- Theatre/Theatre/Source.cpp
+++ Theatre/Theatre/Source.cpp
@@ -9,9 +9,15 @@ int main() {
 	int prices[15];
 
 	do {
+		cout << "Enter the number of your choice: \n";
+		cout << "1. Show available seating\n";
+		cout << "2. Purchase tickets\n";
+		cout << "3. Check availability by row\n";
+		cout << "4. Check total availability\n";
+		cout << "5. Check total sales\n";
+		cout << "6. Quit";
+	} while (userChoice != 6);
 
-	} while (userChoice != 5);
 
-
-
+	return 0;
 }
