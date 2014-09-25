9616d8d559c34ed958dd6d167d206980b97b215d
Eric <ericmlazarski@lewisu.edu>
2014-09-25 13:32:48 -0500

    Wrote majority of readPrices()
    Need to properly implement saving prices to file still
    have not tested reading prices from file yet

--- Theatre/Theatre/Source.cpp
+++ Theatre/Theatre/Source.cpp
@@ -3,31 +3,26 @@
 #include <fstream>
 using namespace std;
 
+void drawSeating(); //declaring seat chart function
+void initSeating();
+void readPrices();
+
+// global variables instead of passing them to functions
+int total = 0;
+float prices[15];
 char seats[30][15];
 
-void drawSeating();//declaring seat chart function
-void initSeating();
-
-
-int total = 0;
-int readPrices();
-
+// main function
 int main() {
 	
+	// stores user input for menu
 	int userChoice;
-	int prices[15];
-
-
-	initSeating();
-	drawSeating();
-
-	
 
 	// initialise seating array
 	initSeating();
 	
-	// find prices
-	
+	// get prices from file or user
+	readPrices();
 
 	do {
 		// draw menu
@@ -38,35 +33,43 @@ int main() {
 		cout << "4. Check total availability\n";
 		cout << "5. Check total sales\n";
 		cout << "6. Quit";
-		// check user input
+		
+		// check user input & respond accordingly
 		cin >> userChoice;
 		switch (userChoice) {
+
 		case 1: // draw chart displaying seating
 			drawSeating();
 			break;
+
 		case 2: // purchase tickets
-			purchaseTickets();
+//			purchaseTickets();
 			break;
+
 		case 3: // check availability of seating by row
-			checkRowAvail();
+	//		checkRowAvail();
 			break;
+
 		case 4: //check availibility of seating in entirety
-			checkTotalAvail();
+		//	checkTotalAvail();
 			break;
+
 		case 5: // display total sales
 			cout << "Total sales: $" << total << endl;
 			break;
+
 		case 6: // quit
 			break;
+
 		default: // invalid input
 			cout << "Unsupported input. Please try again.\n";
+			break;
+
 		}
 	} while (userChoice != 6);
 	
 
 	return 0;
-
-
 
 }
 
@@ -124,7 +127,65 @@ void drawSeating() {
 }
 
 // get prices from user or from file
-int readPrices() {
+void readPrices() {
 
+	ifstream inFile;
+	ofstream outFile;
+	// ask user if they would like to read from a file or enter prices manually
+	int userChoice = 0;;
+	do {
+		// get user input
+		cout << "Enter the number of your choice: " << endl;
+		cout << "1. Enter prices manually\n";
+		cout << "2. Read prices from file\n";
+		cin >> userChoice;
+		// decide what to do based upon input
+		switch (userChoice) {
+
+		case 1: // enter prices manually
+			for (int i = 0; i < 15; i++) {
+				cout << "Enter the price for seating in Row " << i + 1 << ": ";
+				cin >> prices[i];
+				cout << endl;
+			}
+
+			// ask user if they would like to save these prices to a file
+			do {
+				cout << "Would you like to save these prices to a file?\n";
+				cout << "1. Yes, save prices to file\n";
+				cout << "2. No, do not save prices to file\n";
+				cin >> userChoice;
+
+				// decide what to do based upon user input
+				switch (userChoice) {
+				case 1: // save prices to file
+					outFile.open("prices.txt");
+					for (int i = 0; i < 15; i++) {
+						outFile << prices[i] << " ";
+					}
+					break;
+				case 2: // do not save files
+					break;
+				default: // invalid input
+					cout << "Invalid input, please try again.\n";
+					break;
+				}
+			} while (userChoice != 1 || userChoice != 2);
+
+			break;
+
+		case 2: // read prices from file
+			inFile.open("prices.txt");
+			for (int i = 0; i < 15; i++) {
+				cin >> prices[i];
+			}
+			break;
+
+		default: // invalid input
+			cout << "Invalid input, please try again.\n";
+			break;
+		}
+
+	} while (userChoice > 2 && userChoice < 1);
 
 }
